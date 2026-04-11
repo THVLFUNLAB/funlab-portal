6ceb11fe89d13c5a7192447fee1f87749bb6860b
Thay Hau <haunn.vietanhschool@gmail.com>
2026-04-11 16:20:30 +0700

    chore: remove temporary compiler test files

--- test_babel.js
+++
@@ -1,7 +0,0 @@
-const Babel = require('@babel/standalone');
-const code = `<div className="test">Hello</div>`;
-try {
-  console.log(Babel.transform(code, { presets: ['react'] }).code);
-} catch (e) {
-  console.error(e);
-}
--- test_sucrase.js
+++
@@ -1,3 +0,0 @@
-const { transform } = require('sucrase');
-const code = `<div className="test">Hello</div>`;
-console.log(transform(code, { transforms: ['jsx'], production: true }).code);
